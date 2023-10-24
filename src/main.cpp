d4423629e63049dc054bbec83224cad28cd61fcb
Ditel252 <Ditel252@gmail.com>
2023-10-24 09:12:10 +0900

    v0.0.3d

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 #include "Ditel_DMOS.hpp"
 
+#define PIN_CROSS_KEY_UP    19
+#define PIN_CROSS_KEY_DOWN  5
+#define PIN_CROSS_KEY_RIGHT 18
+#define PIN_CROSS_KEY_LEFT  17
+
 DOS os;
 
 void setup(){
+    os.setMainButton(PIN_CROSS_KEY_LEFT, PIN_CROSS_KEY_DOWN, PIN_CROSS_KEY_RIGHT, true);
     os.start();
 }
 
